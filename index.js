0776c83ae152d27994ce8fe81ed35695640d69de
zeke <zeke@sikelianos.com>
2014-01-11 15:36:36 -0800

    maybe __dirname does the trick

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var wikipedias = module.exports = {}
 
-JSON.parse(fs.readFileSync('./data.json')).map(function (w) {
+JSON.parse(fs.readFileSync(__dirname + '/data.json')).map(function (w) {
   wikipedias[w.code] = w;
 })
